0d72a56fc4ad7bb82eede401fce75309d67d091c
Peter Berry <pwcberry@gmail.com>
2025-10-23 17:08:40 +1100

    WIP

--- test/lib/turndown.js
+++ test/lib/turndown.js
@@ -22,18 +22,22 @@ describe("turndown.js", () => {
         td.reset();
     });
 
-    it("should throw an error if the node is not found", () => {
-        JSDOM.fromFile(pathLib.join(fixtureFolder, "index.html")).then((dom) => {
-            const node = dom.querySelector(".unknown");
-            expect(module.turndown(node)).to.throw("Start element does not exist");
+    describe("turndown", () => {
+        // it("should throw an error if the node is not found", () => {
+        //     JSDOM.fromFile(pathLib.join(fixtureFolder, "index.html")).then((dom) => {
+        //         const node = dom.querySelector(".unknown");
+        //         expect(module.turndown(node)).to.throw("Start element does not exist");
+        //     });
+        // });
+
+        it('should call the "downloadFile" function', () => {
+            JSDOM.fromFile(pathLib.join(fixtureFolder, "index.html")).then((dom) => {
+                const node = dom.querySelector("h1");
+                module.turndown(node);
+                expect(utilModule.downloadFile).to.have.been.called();
+            });
         });
     });
 
-    it('should call the "downloadFile" function', () => {
-        JSDOM.fromFile(pathLib.join(fixtureFolder, "index.html")).then((dom) => {
-            const node = dom.querySelector("h1");
-            module.turndown(node);
-            expect(utilModule.downloadFile).to.have.been.called();
-        });
-    });
+    describe("Collector", () => {});
 });
